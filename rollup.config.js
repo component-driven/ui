136d9952b1603ca8a8b997562bdce41417176855
Andrey Okonetchnikov <andrey@okonet.ru>
2018-08-14 00:14:59 +0200

    fix(build): Transpile ESNext code. Add external-helpers

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,19 @@
 import babel from "rollup-plugin-babel"; // eslint-disable-line
 
-const sharedConfig = {
+const env = process.env.NODE_ENV
+
+const config = {
+  input: 'index.js',
+  output: {},
   plugins: [
     babel({
-      exclude: 'node_modules/**' // only transpile our source code
+      exclude: 'node_modules/**', // only transpile our source code
+      plugins: ['external-helpers']
     })
   ],
   external: ['react', 'prop-types']
 }
 
-export default [
-  Object.assign({}, sharedConfig, {
-    input: 'index.js',
-    output: {
-      file: 'build/bundle.js',
-      format: 'cjs'
-    }
-  }),
-  Object.assign({}, sharedConfig, {
-    input: 'index.js',
-    output: {
-      file: 'build/bundle.m.js',
-      format: 'esm'
-    }
-  })
-]
+config.output.format = env
+
+export default config
